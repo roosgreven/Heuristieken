5be8aba41a15fd70f98a3b4ceb6c7d9bf7409c28
nathhje <n.van.sburg@hotmail.nl>
2018-04-17 12:27:16 +0200

    update, grid werkt

--- houses.py
+++ houses.py
@@ -1,10 +1,51 @@
-ground = []
 
-# create grid for the neighbourhood
-for i in range(30):
-    ground.append([])
-    for j in range(32):
-        ground[i].append(0)
+totalHouses = 20
+totalEengezins = int(totalHouses * 0.6)
+totalBungalow = int(totalHouses * 0.25)
+totalMaison = int(totalHouses * 0.15)
+print totalEengezins
+print totalBungalow
+print totalMaison
+ground = []
+houses = []
+
+def main():
+    
+    # create grid for the neighbourhood
+    for i in range(50):
+        ground.append([])
+        for j in range(50):
+            ground[i].append(0)
+    
+    for i in range(totalEengezins):
+        houses.append(eengezins())
+    
+    for i in range(totalBungalow):
+        houses.append(bungalow())
+        
+    for i in range(totalMaison):
+        houses.append(maison())
+        
+    for house in houses:
+        switch = False
+        for i in range(50):
+            for j in range(50):
+                if(house.checkIfEmpty(j,i)):
+                    house.placeObject(j,i)
+                    
+                    switch = True
+                    break
+            if switch == True:
+                break
+    
+    """"
+    one = eengezins()
+    if(one.checkIfEmpty(2,2)):
+        one.placeObject(2,2)
+    """
+    for row in ground:
+        print row
+
 
 # class for houses. Contains functions to calculate total value of house and to place a house if there's enough space. 
 class house:
@@ -20,9 +61,10 @@ class house:
         # check if there's enough space for the house. If not, return from function.
         for meterWidth in range(positionX - self.freeSpace, self.width + self.freeSpace * 2 + positionX):
             for meterLength in range(positionY - self.freeSpace, self.length + self.freeSpace * 2 + positionY):
-                if(ground[meterLength][meterWidth] != 0):
-                    print("false")
-                    return False
+                if(meterLength > 50 - self.freeSpace or meterWidth > 50 - self.freeSpace): 
+                    return
+                elif(ground[meterLength][meterWidth] != 0):
+                        return False
         
         return True
     
@@ -51,28 +93,28 @@ class house:
 
 class eengezins(house):
     theType = 1
-    width = 8
-    length = 8
+    width = 3
+    length = 3
     basicValue = 285000
-    freeSpace = 2
+    freeSpace = 1
     freeSpaceType = 2
     extraValue = 0.3 * 285000
 
 class bungalow(house):
     theType = 3
-    width = 10
-    length = 7
+    width = 5
+    length = 4
     basicValue = 399000
-    freeSpace = 3
+    freeSpace = 1
     freeSpaceType = 4
     extraValue = 0.4 * 399000
 
 class maison(house):
     theType = 5
-    width = 11
-    length = 10
+    width = 7
+    length = 6
     basicValue = 610000
-    freeSpace = 6
+    freeSpace = 1
     freeSpaceType = 6
     extraValue = 0.6 * 610000
 
@@ -84,14 +126,5 @@ class water(house):
         self.width = width
         self.length = length
         
-totalHouses = 20
-totalEengezins = totalHouses * 0.6
-totalBungalow = totalHouses * 0.25
-totalMaison = totalHouses * 0.15    
-
-one = eengezins()
-if(one.checkIfEmpty(2,2)):
-    one.placeObject(2,2)
-    
-for row in ground:
-    print row
+if __name__ == "__main__":
+    main()
