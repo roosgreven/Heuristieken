25440cd4c9b1ef1f739582621fd8dd5223916b26
roosgreven <roosgreven12@hotmail.com>
2018-04-24 12:41:40 +0200

    added functions to main

--- code/coordinaten_nathalie/main.py
+++ code/coordinaten_nathalie/main.py
@@ -3,37 +3,43 @@ import shortestdistance as sd
 import random
 import matplotlib.pyplot as plt
 
+def findClosestHouse(houses, chosenHouse):
+    distance = 160
+    closestHouse = houses[0]
+    
+    for i in range(len(houses)):
+        if i == closestHouse:
+            print('hallo')
+        if(sd.Shortest(chosenHouse, houses[i]) < distance):
+            distance = sd.Shortest(chosenHouse, houses[i])
+            closestHouse = houses[i]
+            
+    print(distance)
+            
 def main():
     
     houses = []
+    numberOfHouses = 20
     
-    for i in range(20):
+    for i in range(numberOfHouses):
         
-        x = random.random() * 160
-        y = random.random() * 180
+        x = round(random.random() * 160, 1)
+        y = round(random.random() * 180, 1)
         
         houses.append(ht.maison(x, y))
-    
-    for i in range(20):
-        
-        print(i)
-        
-        for j in range(20):
-            
-            if not i == j:
-                
-                distance = sd.Shortest(houses[i], houses[j])
-                houses[i].value(distance)
-                print(houses[i].totalValue)
-                
+                   
     xlist = [house.x1 for house in houses]
     ylist = [house.y1 for house in houses]
-    print xlist
-    print ylist
+    print(xlist)
+    print(ylist)
     
     plt.figure()
     plt.plot(xlist, ylist, 'bo')
     plt.show()
     
+    findClosestHouse(houses, houses[0])
+    
+    
 if __name__ == "__main__":
     main()
+    
