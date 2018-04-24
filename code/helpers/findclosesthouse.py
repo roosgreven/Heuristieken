f0889bcd51cf0ded2c331206df72c729af41a343
roosgreven <roosgreven12@hotmail.com>
2018-04-24 17:32:54 +0200

    added comments

--- code/helpers/findclosesthouse.py
+++ code/helpers/findclosesthouse.py
@@ -1,17 +1,36 @@
 import helpers.shortestdistance as sd
 
+"""
+Finds distance between given house and house closest to that house. 
+Takes in the array of placed houses and the house to check. 
+Returns false if there's overlap between houses. 
+Returns distance if there's no overlap. 
+"""
+
 def findClosestHouse(houses, chosenHouse):
 
+    # set distance to fake number
     distance = 160
+
+    # loop over all houses
     for i in range(len(houses)):
+
+        # check if house is not chosen house
         if(chosenHouse.x1 != houses[i].x1 and chosenHouse.x2 != houses[i].x2):
+
+            # check for overlap
             if(sd.Shortest(chosenHouse, houses[i]) > chosenHouse.freeSpace):
+
+                # check if distance between two houses is smaller than previous smallest distance
                 if(sd.Shortest(chosenHouse, houses[i]) < distance):
+
+                    # save new smallest distance
                     distance = sd.Shortest(chosenHouse, houses[i])
                     closestHouse = houses[i]
+
+            # return false if there's overlap
             else:
                 return False
 
-    chosenHouse.value(distance)
-
+    # return smallest distance
     return(distance)
--- code/main.py
+++ code/main.py
@@ -11,25 +11,31 @@ def main():
     numberOfHouses = 20
     i = 0
     overlaps = 0
-    
+
+    # loop over houses
     while i < numberOfHouses:
-        
+
+        # get random coordinates
         x = round(random.random() * 160, 1)
         y = round(random.random() * 180, 1)
-        
+
+        # add house placed randomly
         houses.append(hs.maison(x, y))
 
+        # check if there's overlap, if so, delete house from array and try again
         if not(fch.findClosestHouse(houses, hs.maison(x, y))):
-
             houses = houses[:-1]
 
+        # add value of house to array and continue if no overlap
         else:
             totalHouseValues.append(houses[i].value(fch.findClosestHouse(houses, houses[i])))
             i += 1
 
+    # add coordinates of houses to list
     xlist = [house.x1 for house in houses]
     ylist = [house.y1 for house in houses]
 
+    # plot visualisation
     plt.figure()
     plt.plot(xlist, ylist, 'bo')
     #plt.show()
