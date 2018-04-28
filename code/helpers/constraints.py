59c8b3cc8c6d8e63472572ed44ff08bdcc5d56ca
Jitse Schol <jitseschol@hotmail.com>
2018-04-28 13:14:25 +0200

    water placement function

--- code/helpers/constraints.py
+++ code/helpers/constraints.py
@@ -24,16 +24,33 @@ def noOverlap(houseArray, chosenHouse):
     # No overlap when there's only one house.
     else:
         return True
-        
+
 def checkBoundaries(plan, house):
     """ Returns False if the house is too close to a boundary. """
-    
+
     # Checks left and upper boundary.
     if house.x1 < house.freeSpace or house.y1 < house.freeSpace:
         return False
-    
+
     # Checks right and lower boundary.
     if house.x2 > plan.width - house.freeSpace or house.y2 > plan.length - house.freeSpace:
         return False
-        
+
     return True
+
+
+def waterPlacement(plan, houseArray, water):
+    """ Returns False if water cannot be placed """
+
+    # Checks whether water fits within area boundaries
+    if water.x2 > plan.width or water.y2 plan.length:
+        return False
+
+    if len(houseArray) > 1:
+
+        # Loop over all houses
+        for house in houseArray
+
+            # Check if water is inside house
+            if sd.shortest(water, house) < 0
+                return False
