405d21f52ea68beb2c0f7b245f6ce94f9e67454e
Jitse Schol <jitseschol@hotmail.com>
2018-05-17 13:42:58 +0200

    rotate werkt nog niet, swap en move wel

--- code/helpers/improvements.py
+++ code/helpers/improvements.py
@@ -6,11 +6,152 @@ Contains functions for the improvements made in hillclimber.  Possibilities:
 swap two houses, rotate a house, move a house.
 """
 
+import classes.houses as hs
+from classes.floorplan import FloorPlan
+import helpers.findclosesthouse as fch
+import helpers.constraints as con
+import random
+import randomalgorithm.randomalgorithm
+
+def randomHouse(houseArray):
+    """ Returns index of random house from house array in floorplan """
+
+    lengthHouseArray = len(houseArray) - 1
+
+    # Random index of given houseArray
+    index = round(random.random() * lengthHouseArray, 1)
+
+    return int(index)
+
+def houseMove(houseToBeMoved, plan, oldValue):
+    """ First checks the value of the plan as it is. Then, moves a house in a
+        random direction, using a distance between 0.0 and 1.0.
+        The function checks whether the house move is viable.
+        After the move has been made, it compares the plan value of the old
+        situation and the new situation.
+        The house will be moved back to its old position if the value has decreased"""
+
+    # Save current coordinates
+    oldx1 = houseToBeMoved.x1
+    oldy1 = houseToBeMoved.y1
+
+    # Retrieved from https://stackoverflow.com/questions/48122608/how-do-i-generate-random-float-and-round-it-to-1-decimal-place
+    movementx = round(int(random.uniform(-1, 1) * 10)) / 10.0
+    movementy = round(int(random.uniform(-1, 1) * 10)) / 10.0
+
+    # New x and y coordinates
+    newx1 = oldx1 + movementx
+    newy1 = oldy1 + movementy
+
+    # Change coordinates of house to be moved
+    houseToBeMoved.coordinates(newx1, newy1)
+
+    # Check if there's overlap with water and boundaries
+    if con.noWaterAndBoundary(houseToBeMoved, plan):
+
+        distance = fch.findClosestHouse(plan, houseToBeMoved)
+
+        if distance > 0:
+
+            newValue = plan.getValue()
+
+            # If the house move has decreased plan value
+            if not newValue >= oldValue:
+
+                # Set house back at old position
+                houseToBeMoved.coordinates(oldx1, oldy1)
+        else:
+            # Set house back at old position
+            houseToBeMoved.coordinates(oldx1, oldy1)
+
+    # If move is not viable, because of overlap
+    else:
+
+        # Set house back at old position
+        houseToBeMoved.coordinates(oldx1, oldy1)
+
 def swap(house1, house2):
     """ Swaps the lower left coordinates of two houses. """
-    
+
     tempX = house2.x1
     tempY = house2.y1
-    
+
     house2.coordinates(house1.x1, house1.y1)
     house1.coordinates(tempX, tempY)
+
+def swapCheck(house1, house2, plan, oldValue):
+    """ Checks if swap was viable for both houses and whether
+        plan value was improved or the same. If not, houses are set back to
+        old positions """
+
+    # Check if there's overlap with water and boundaries for house 1
+    if con.noWaterAndBoundary(house1, plan):
+
+        distance = fch.findClosestHouse(plan, house1)
+
+        if distance > 0:
+
+            newValue = plan.getValue()
+
+            # If the house move has decreased plan value
+            if not newValue >= oldValue:
+                swap(house2, house1)
+                return 0
+        else:
+            swap(house2, house1)
+            return 0
+    # If move is not viable, because of overlap
+    else:
+        swap(house2, house1)
+        return 0
+
+    # Check if there's overlap with water and boundaries for house 2
+    if con.noWaterAndBoundary(house2, plan):
+
+        distance = fch.findClosestHouse(plan, house2)
+
+        if distance > 0:
+
+            newValue = plan.getValue()
+
+            # If the house move has decreased plan value
+            if not newValue >= oldValue:
+                swap(house2, house1)
+                return 0
+        else:
+            swap(house2, house1)
+            return 0
+    # If move is not viable, because of overlap
+    else:
+        swap(house2, house1)
+        return 0
+
+def rotateHouse(house, plan, oldValue):
+    print(house.x1)
+    print(house.y1)
+    house.rotate()
+    print(house.x1)
+    print(house.y1)
+    print("house rotated")
+    # Check if there's overlap with water and boundaries
+    if con.noWaterAndBoundary(house, plan):
+
+        distance = fch.findClosestHouse(plan, house)
+
+        if distance > 0:
+
+            newValue = plan.getValue()
+
+            # If the house rotation has decreased plan value
+            if not newValue >= oldValue:
+
+                # Rotate house back
+                house.rotate()
+        else:
+            # Rotate house back
+            house.rotate()
+
+    # If move is not viable, because of overlap
+    else:
+        # Rotate house back
+        house.rotate()
--- code/hillclimber/hillclimber.py
+++ code/hillclimber/hillclimber.py
@@ -12,82 +12,64 @@ import helpers.findclosesthouse as fch
 import helpers.constraints as con
 import random
 import randomalgorithm.randomalgorithm
+import helpers.improvements as imp
 
 def hillClimber(plan):
 
     # Initiate counter
     i = 0
 
-    for i in range(3000):
+    for i in range(30):
 
-        # Select random house in houses array of current plan
-        index = randomHouse(plan.houses)
+        # Get value of plan as it is now
+        oldValue = plan.getValue()
 
-        # Move house in random direction with houseMove function
-        houseMove(plan.houses[index], plan)
+        # Random number between 0 and 1, rounded to 2 decimals
+        whichMove = round(random.random(), 2)
+
+        # 50% chance that it is a coordinate change of a single house
+        if whichMove <= 0.50:
+
+            # Select random house in houses array of current plan
+            index = imp.randomHouse(plan.houses)
+
+            # Move a house in random direction with houseMove function
+            imp.houseMove(plan.houses[index], plan, oldValue)
+
+        # 25% chance that the move is a swap of two houses
+        elif whichMove <= 0.75:
+
+            # Select random house in houses array of current plan as index 1
+            index1 = imp.randomHouse(plan.houses)
+
+            # Select random house in houses array of current plan as index 2
+            index2 = imp.randomHouse(plan.houses)
+
+            # While house indexes are the same
+            while(index1 == index2):
+
+                # Call random house function for new index2
+                index2 = imp.randomHouse(plan.houses)
+
+
+            # Call swap function that swaps two houses
+            imp.swap(plan.houses[index1], plan.houses[index2])
+
+            # Check if swap was viable, if not, this function sets houses back
+            imp.swapCheck(plan.houses[index1], plan.houses[index2], plan, oldValue)
+
+        # 25% chance that the move is a rotation of a single house
+        else:
+
+            # Select random house in houses array of current plan
+            index = imp.randomHouse(plan.houses)
+
+            imp.rotateHouse(plan.houses[index], plan, oldValue)
 
     print("hillclimber succeeded")
     return plan
 
-def randomHouse(houseArray):
-    """ Returns index of random house from house array in floorplan """
 
-    lengthHouseArray = len(houseArray) - 1
-
-    # Random index of given houseArray
-    index = round(random.random() * lengthHouseArray, 1)
-
-    return int(index)
-
-def houseMove(houseToBeMoved, plan):
-    """ First checks the value of the plan as it is. Then, moves a house in a
-        random direction, using a distance between 0.0 and 1.0.
-        The function checks whether the house move is viable.
-        After the move has been made, it compares the plan value of the old
-        situation and the new situation.
-        The house will be moved back to its old position if the value has decreased"""
-
-    # Get value of plan as it is now
-    oldValue = plan.getValue()
-
-    # Save current coordinates
-    oldx1 = houseToBeMoved.x1
-    oldy1 = houseToBeMoved.y1
-
-    # Retrieved from https://stackoverflow.com/questions/48122608/how-do-i-generate-random-float-and-round-it-to-1-decimal-place
-    movementx = round(int(random.uniform(-1, 1) * 10)) / 10.0
-    movementy = round(int(random.uniform(-1, 1) * 10)) / 10.0
-
-    # New x and y coordinates
-    newx1 = oldx1 + movementx
-    newy1 = oldy1 + movementy
-
-    # Change coordinates of house to be moved
-    houseToBeMoved.coordinates(newx1, newy1)
-
-    # Check if there's overlap with water and boundaries
-    if con.noWaterAndBoundary(houseToBeMoved, plan):
-
-        distance = fch.findClosestHouse(plan, houseToBeMoved)
-
-        if distance > 0:
-
-            newValue = plan.getValue()
-
-            # If the house move has decreased plan value
-            if not newValue >= oldValue:
-
-                # Set house back at old position
-                houseToBeMoved.coordinates(oldx1, oldy1)
-        else:
-            # Set house back at old position
-            houseToBeMoved.coordinates(oldx1, oldy1)
-
-    # If move is not viable, because of overlap
-    else:
-
-        # Set house back at old position
-        houseToBeMoved.coordinates(oldx1, oldy1)
 
 if __name__ == "__main__":
     plan = hillClimber(20)
